54c0eab2d196c46e63c40e39559b5b634958844e
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-07-06 21:20:49 +0530

    Add files via upload

---
+++ .jinn.sh
@@ -0,0 +1,13 @@
+#!data/data/com.termux/files/usr/bin/bash
+clear
+cd server
+echo '''
+     ₹____________________<😈>_____________________₹
+             Repository master : MR JINN
+       [ Telegram : https://t.me/MrJinnTelegram ]
+      ₹____________________________________________₹
+''' | lolcat
+
+echo "Please wait..."
+
+node index.js
