a6f1fe68ac7bf79e5938b30343bbaf539e9e0793
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-08-27 13:12:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -54,7 +54,7 @@ echo " "
 
         exit
     elif [ $ch -eq 4 ];then
-        echo -e "\e[1;31m 😈 Apk copy sdcard please...📌"
+        echo -e "\e[1;31m 😈 Apk copy sdcard Please check...📌"
         cd $HOME
         cd JINNRAT
         cd .RAT
